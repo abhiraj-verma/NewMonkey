4f5a34174949889bc15925c3b8b604ce53ab16df
abhiraj.verma <abhiraj.verma721@gmail.com>
2025-03-10 20:53:46 +0530

    Added new monket initial commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
+    return (
+      <div>
+        <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://image.cnbcfm.com/api/v1/image/108037366-1726860624562-34th_Street_4.jpg?v=1726861161&w=1920&h=1080"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="test-muted">
+                {" "}
+                by {author ? author : "unknown"}
+                on {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              rel="noreferrer"
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
